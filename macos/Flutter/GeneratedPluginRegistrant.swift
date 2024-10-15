3795ec1f6a78f2c2ba036155704232539f5bf4dc
juniperus01 <jiyabhagat402@gmail.com>
2024-10-15 13:51:33 +0530

    Resolving conflicts

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,13 +5,19 @@
 import FlutterMacOS
 import Foundation
 
+<<<<<<< Updated upstream
 import cloud_firestore
+=======
+>>>>>>> Stashed changes
 import firebase_auth
 import firebase_core
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+<<<<<<< Updated upstream
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+=======
+>>>>>>> Stashed changes
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
